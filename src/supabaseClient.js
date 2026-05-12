51ebe8c1042a7a5fd81e57facd3bb2ddfde61d36
Khofi Alfian <136216871+Freezycoffee@users.noreply.github.com>
2026-05-12 20:33:57 +0700

    Delete src directory

--- src/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-
-const supabaseUrl = process.env.VUE_APP_SUPABASE_URL;
-const supabasePublishableKey = process.env.VUE_APP_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
-
-export const supabase = createClient(supabaseUrl, supabasePublishableKey)
